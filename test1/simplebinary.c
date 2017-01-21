975f603b4c33f066358774bec6a7250b6031c9d8
Gábor Siffel <gaborsiffelpro@gmail.com>
2017-01-21 01:14:04 -0500

    pls

--- test1/simplebinary.c
+++ test1/simplebinary.c
@@ -2,19 +2,19 @@
 #include "simplebinary.h"
 
 // returns an "array" of length 8
-int* freq_8_from_input(char one_byte) {
+int* freq_8_from_input(unsigned char one_byte) {
     static int a[8];
     for (int i = 0; i < 8; ++i) {
-        a[i] = 400 + ((one_byte >> (7 - i)) & 0x1) * 400;
+        a[i] = 400 + (unsigned char) ((one_byte >> (7 - i)) & 0x1) * 400;
     }
     return a;
 }
 
 // returns an "array" of length 4
-int* freq_4_from_input(char one_byte) {
+int* freq_4_from_input(unsigned char one_byte) {
     static int a[4];
     for (int i = 0; i < 4; ++i) {
-        a[i] = 300 + ((one_byte >> (7 - (i * 2))) & 0x3) * 200;
+        a[i] = 300 + (unsigned char) ((one_byte >> (6 - (i * 2))) & 0x3) * 200;
     }
     return a;
 }
--- test1/test1.c
+++ test1/test1.c
@@ -4,5 +4,11 @@
 #include "simplebinary.h"
 
 int main() {
-    printf("oh my: %i", freq_8_from_input(0x1)[7]);
+    printf("this number should be 400: %i\n", freq_8_from_input(0x1)[6]);
+    printf("this number should be 800: %i\n", freq_8_from_input(0x1)[7]);
+
+    printf("this number should be 300: %i\n", freq_4_from_input(0x0)[3]);
+    printf("this number should be 500: %i\n", freq_4_from_input(0x1)[3]);
+    printf("this number should be 700: %i\n", freq_4_from_input(0x2)[3]);
+    printf("this number should be 900: %i\n", freq_4_from_input(0x3)[3]);
 }
