1bfe434f45487f76314f0acf27c0074ba6cd3c06
Gábor Siffel <gaborsiffelpro@gmail.com>
2017-01-21 13:01:41 -0500

    upgraded encode

--- Sound Transfer/encode.c
+++ Sound Transfer/encode.c
@@ -2,10 +2,18 @@
 #include "globals.h"
 #include "encode.h"
 
-void freqs_from_input(char* data, int num_of_bytes, int* output) {
+int* freqs_from_input(char* data, int num_of_bytes) {
+    int output[num_of_bytes * 8 / BITS_PER_TONE];
     for (int b = 0; b < num_of_bytes; ++b) {
         for (int i = 0; i < 8 / BITS_PER_TONE; ++i) {
-            output[b * (8 / BITS_PER_TONE) + i] = BASE_FREQ + (unsigned char) (((*data + b) >> (8 - BITS_PER_TONE - (i * BITS_PER_TONE))) & ((0x1 << (BITS_PER_TONE + 1)) - 1)) * LINEAR_INTERVAL;
+            output[b * (8 / BITS_PER_TONE) + i] = BASE_FREQ
+                + (unsigned char) (
+
+//                   [cur byte ]    [          section of byte              ]    [         bit mask               ]
+                    ((*data + b) >> (8 - BITS_PER_TONE - (i * BITS_PER_TONE))) & ((0x1 << (BITS_PER_TONE + 1)) - 1)
+
+                ) * LINEAR_INTERVAL;
         }
     }
+    return output;
 }
--- Sound Transfer/encode.h
+++ Sound Transfer/encode.h
@@ -1,2 +1,2 @@
 
-void freqs_from_input(char* data, int num_of_bytes, int* output);
+int* freqs_from_input(char* data, int num_of_bytes);
