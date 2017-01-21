c9b04c1dac79c61629d4e256fc3baaa0ab754f0e
Gábor Siffel <gaborsiffelpro@gmail.com>
2017-01-21 00:47:27 -0500

    static i guess

--- test1/simplebinary.c
+++ test1/simplebinary.c
@@ -3,7 +3,7 @@
 
 // returns an "array" of length 8
 int* freq_8_from_input(char one_byte) {
-    int[8] a;
+    static int[8] a;
     for (int i = 0; i < 8; ++i) {
         a[i] = 400 + ((one_byte >> (7 - i)) & 0x1) * 400;
     }
@@ -12,7 +12,7 @@ int* freq_8_from_input(char one_byte) {
 
 // returns an "array" of length 4
 int* freq_4_from_input(char one_byte) {
-    int[4] a;
+    static int[4] a;
     for (int i = 0; i < 4; ++i) {
         a[i] = 300 + ((one_byte >> (7 - (i * 2))) & 0x3) * 200;
     }
