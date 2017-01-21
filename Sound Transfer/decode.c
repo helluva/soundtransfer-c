16a6d050d9ef9dc9ad3399a50440cb4bd7582b5a
Gábor Siffel <gaborsiffelpro@gmail.com>
2017-01-21 12:43:58 -0500

    whoopdy doo

--- Sound Transfer/decode.c
+++ Sound Transfer/decode.c
@@ -1,29 +1,27 @@
 
+#include "globals.h"
 #include "decode.h"
 #include <math.h>
 #include <stdlib.h>
 
-static const int Uninitialized = -99;
-static const int Not_Receiving = 0;
-static const int Transfer_Complete = -1;
+static const int UNINITIALIZED = -99;
+static const int NOT_RECEIVING = -1;
+static const int RECEIVING_HEADER = 1;
+static const int TRANSFER_COMPLETE = 0;
 
-static int statusCode = Uninitialized;
+static int statusCode = UNINITIALIZED;
 
 
-static const double Samples_Per_Second = 36.0;
-static const double Encoded_Frequencies_Per_Second = 4.0;
-static const double Samples_Per_Encoded_Frequency = 9;
 
-static const double Initialize_Transfer_Frequency = 1500.0;
 
 
 static double* previousFrequencies;
 static int previousFrequenciesCount = 0;
 
 
-void initialize() {
-    statusCode = Not_Receiving;
-    previousFrequencies = malloc(sizeof(double) * Samples_Per_Encoded_Frequency);
+void init(int* length, char** decodedBytes) {
+    statusCode = NOT_RECEIVING;
+    previousFrequencies = malloc(sizeof(double) * SAMPLES_PER_ENCODED_FREQUENCY);
 }
 
 
@@ -34,38 +32,38 @@ int frequenciesMatch(double frequency, double target) {
 }
 
 
-int frame(double frequency, char** decodedBytes) {
-    
-    if (statusCode == Uninitialized) {
+int receive_frame(double frequency) {
+
+    if (statusCode == UNINITIALIZED) {
         return statusCode;
     }
-    
-    
+
+
     //build the 9 previous frequencies
-    if (previousFrequenciesCount < Samples_Per_Encoded_Frequency) {
+    if (previousFrequenciesCount < SAMPLES_PER_ENCODED_FREQUENCY) {
         previousFrequencies[previousFrequenciesCount] = frequency;
         previousFrequenciesCount += 1;
     }
-    
-    if (previousFrequenciesCount == Samples_Per_Encoded_Frequency) {
-        
+
+    if (previousFrequenciesCount == SAMPLES_PER_ENCODED_FREQUENCY) {
+
         //build "dictionary" of valid frequencies
         int validFrequenciesCount = 16;
         int expectedFrequencies[validFrequenciesCount];
         int matchCount[validFrequenciesCount];
-        
+
         int base = 1000; //replace with global constant
         int increment = 100; //replace with global constant
-        
+
         for (int i = 0; i < validFrequenciesCount; i++) {
             matchCount[i] = 0;
             expectedFrequencies[i] = base + (increment * i);
         }
-        
+
         //attribute previous frequencies to valid frequencies
         for (int i = 0; i < previousFrequenciesCount; i++) {
             int frequencyToAnalyze = previousFrequencies[i];
-            
+
             for (int j = 0; j < validFrequenciesCount; j++){
                 int possibleMatch = expectedFrequencies[j];
                 if (frequenciesMatch(frequencyToAnalyze, possibleMatch)) {
@@ -73,29 +71,29 @@ int frame(double frequency, char** decodedBytes) {
                 }
             }
         }
-        
+
         //find most common of the expected frequencies
         int mostCommonFrequency = -1;
         int mostCommonFrequencyCount = 0;
-        
+
         for (int j = 0; j < validFrequenciesCount; j++) {
             if (matchCount[j] > mostCommonFrequencyCount) {
                 mostCommonFrequency = expectedFrequencies[j];
                 mostCommonFrequencyCount = matchCount[j];
             }
         }
-        
-        
+
+
         //reset previous frequencies
         previousFrequenciesCount = 0;
-        for (int i = 0; i < Samples_Per_Encoded_Frequency; i++) {
+        for (int i = 0; i < SAMPLES_PER_ENCODED_FREQUENCY; i++) {
             previousFrequencies[i] = 0;
         }
-        
+
         if (mostCommonFrequency != -1 && mostCommonFrequencyCount > 4) {
             return mostCommonFrequency;
         }
     }
-    
+
     return statusCode;
 }
--- Sound Transfer/decode.h
+++ Sound Transfer/decode.h
@@ -1,9 +1,9 @@
 
 
-void initialize();
+void init(int* length, char** decodedBytes);
 
 // return 0: not receiving data
 // return n: receiving data of byte length n
 // return -1: transfer complete
 
-int frame(double frequency, char** decodedBytes);
+int receive_frame(double frequency);
