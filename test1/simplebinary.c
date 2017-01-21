61eac19d1b606d8986bd0eb0c36a4e5fbc46e61b
Gábor Siffel <gaborsiffelpro@gmail.com>
2017-01-21 01:00:09 -0500

    oh my

--- test1/simplebinary.c
+++ test1/simplebinary.c
@@ -3,7 +3,7 @@
 
 // returns an "array" of length 8
 int* freq_8_from_input(char one_byte) {
-    static int[8] a;
+    static int a[8];
     for (int i = 0; i < 8; ++i) {
         a[i] = 400 + ((one_byte >> (7 - i)) & 0x1) * 400;
     }
@@ -12,7 +12,7 @@ int* freq_8_from_input(char one_byte) {
 
 // returns an "array" of length 4
 int* freq_4_from_input(char one_byte) {
-    static int[4] a;
+    static int a[4];
     for (int i = 0; i < 4; ++i) {
         a[i] = 300 + ((one_byte >> (7 - (i * 2))) & 0x3) * 200;
     }
---
+++ test1/test1.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+#include "simplebinary.h"
+
+int main() {
+    printf("oh my: %i", freq_8_from_input(0x1)[7]);
+}
