6f391a33f50271111ab64a407471c5cba4d29557
Gábor Siffel <gaborsiffelpro@gmail.com>
2017-01-21 14:26:26 -0500

    potential fix

--- Sound Transfer/encode.c
+++ Sound Transfer/encode.c
@@ -11,8 +11,8 @@ int* freqs_from_input(const char* data, int num_of_bytes) {
             output[b * (8 / BITS_PER_TONE) + i] = BASE_FREQ
                 + (unsigned char) (
 
-//                   [cur byte ]    [          section of byte              ]    [         bit mask               ]
-                    ((*data + b) >> (8 - BITS_PER_TONE - (i * BITS_PER_TONE))) & ((0x1 << (BITS_PER_TONE + 1)) - 1)
+//                   [cur byte ]    [          section of byte              ]    [      bit mask            ]
+                    ((*data + b) >> (8 - BITS_PER_TONE - (i * BITS_PER_TONE))) & ((0x1 << BITS_PER_TONE) - 1)
 
                 ) * LINEAR_INTERVAL;
         }
