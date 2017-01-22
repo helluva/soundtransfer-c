00d33ca00096a4ff1d54a9b01159bc6eb9113348
rohunatluri <rohun.atluri@gmail.com>
2017-01-21 20:08:12 -0500

    support

--- Sound Transfer/encode.c
+++ Sound Transfer/encode.c
@@ -1,15 +1,16 @@
 
 #include <stdlib.h>
-
+#include <stdio.h>
 #include "globals.h"
 #include "encode.h"
 
+
 int* freqs_from_input(const char* data, int num_of_bytes) {
     char numbytes = num_of_bytes;
-    int* output = malloc(sizeof(int) * (3 + num_of_bytes * 8 / BITS_PER_TONE));
+    int* output = malloc(sizeof(int) * (4 + num_of_bytes * 8 / BITS_PER_TONE));
     for (int b = 0; b < num_of_bytes; ++b) {
         for (int i = 0; i < 8 / BITS_PER_TONE; ++i) {
-            output[3 + b * (8 / BITS_PER_TONE) + i] = BASE_FREQ
+            output[4 + b * (8 / BITS_PER_TONE) + i] = BASE_FREQ
                 + ((unsigned char) (
 
 //                                    [cur byte ]     [          section of byte              ]                      [      bit mask            ]
@@ -18,12 +19,26 @@ int* freqs_from_input(const char* data, int num_of_bytes) {
                 )) * LINEAR_INTERVAL;
         }
     }
+
+    int x = BASE_FREQ
+        + ((unsigned char) (
+
+//                                    [cur byte ]     [          section of byte              ]                      [      bit mask            ]
+            (((unsigned char) ((num_of_bytes >> 4) & 0xF)) >> (8 - BITS_PER_TONE - (0 * BITS_PER_TONE))) & (((unsigned char) (0x1 << BITS_PER_TONE)) - 1)
+
+        )) * LINEAR_INTERVAL;
+
+    int y = BASE_FREQ
+        + ((unsigned char) (
+
+//                                    [cur byte ]     [          section of byte              ]                      [      bit mask            ]
+            (((unsigned char) (num_of_bytes & 0xF)) >> (8 - BITS_PER_TONE - (1 * BITS_PER_TONE))) & (((unsigned char) (0x1 << BITS_PER_TONE)) - 1)
+
+        )) * LINEAR_INTERVAL;
+
     output[0] = GUARD_FREQUENCY;
     output[1] = GUARD_FREQUENCY_B;
-    output[2] = numbytes & 0xFF;
+    output[2] = x;
+    output[3] = y;
     return output;
-}
-
-int main() {
-    return 0;
 }
